d3a1e28c662e3a7c70911b6f5383837cb9c9e5b2
dsmirnov8080 <dsmirnov8080@gmail.com>
2016-03-19 01:07:46 +0600

    fix

--- backend/controllers/ClientController.php
+++ backend/controllers/ClientController.php
@@ -5,3 +5,22 @@
  * Date: 18.03.2016
  * Time: 15:44
  */
+
+namespace backend\controllers;
+
+use Yii;
+//use backend\models\post;
+/*
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use common\models\LoginForm;
+use yii\filters\VerbFilter;
+*/
+
+/**
+ * Site controller
+ */
+class ClientController extends Controller
+{
+    
+}
